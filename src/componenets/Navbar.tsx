d0fc17ad63f9269be7eb9877dd8bdd6b70ee463f
shaky008 <sshakya.uutsav@gmail.com>
2023-09-09 20:19:27 -0700

    added new logo and refresh page when clicked

--- src/componenets/Navbar.tsx
+++ src/componenets/Navbar.tsx
@@ -1,11 +1,21 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/barrel-svgrepo-com.svg";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchBar from "./SearchBar";
 const Navbar = () => {
+  // Reload the current page
+  const handleRefreshClick = () => {
+    window.location.reload();
+  };
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image
+        src={logo}
+        boxSize="60px"
+        onClick={handleRefreshClick}
+        cursor="pointer"
+        alt="website"
+      />
       <SearchBar />
       <ColorModeSwitch />
     </HStack>
