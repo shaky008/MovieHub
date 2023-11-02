6fbe2b6dc6926f03017278f9355a33c85716e6e0
shaky008 <90487676+shaky008@users.noreply.github.com>
2023-11-01 23:27:07 -0700

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "1b150dce7f8509ba675f68a56212d6b3",
+    //api_key: YOUR API KEY HERE ,
   },
 });
